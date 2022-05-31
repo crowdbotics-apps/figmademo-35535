c2f44e6fcb61abdb182480f19e4cc81ba1e106ac
Crowdbotics <team@crowdbotics.com>
2022-05-31 21:15:09 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,14 @@
+from django.conf import settings
 from django.db import models
+
+
+class Order(models.Model):
+    "Generated Model"
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="order_user",
+    )
+    order_number = models.CharField(
+        max_length=256,
+    )
